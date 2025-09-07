00dbcc155ac6d177cad35b2bd100f451a8951420
Manuel Spigolon <behemoth89@gmail.com>
2025-09-07 08:36:15 +0200

    chore: suggested linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
+import reactDom from "eslint-plugin-react-dom";
+import react from "eslint-plugin-react-x";
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
@@ -12,6 +14,8 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      reactDom.configs.recommended,
+      react.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
